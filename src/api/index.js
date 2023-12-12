064d0a6f2dd15fb3bf9aab476e0095283a5f22e6
Chetan Pravin Sonawane <chetanps131@gmail.com>
2023-12-12 20:12:14 +0530

    added backend

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({ 
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://stackoverflow-backend-j1oq.onrender.com'
 });
 
 API.interceptors.request.use((req) => {
